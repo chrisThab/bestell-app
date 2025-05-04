6f250e7b16e02ba4511a91468f981879e6401a89
chrisThab <christianthabar@gmail.com>
2025-05-04 16:37:13 +0200

    start der bestell-app

---
+++ script/db.js
@@ -0,0 +1,53 @@
+let dishes = [
+    {
+        "name": "Gebratete Nudeln",
+        "price": 9.80,
+        "desription": "Nudeln gebraten mit Gemüse, optional Huhn, Rind, Schwein oder Garnelen"
+    },
+    {
+        "name": "Pizza Margeritha",
+        "price": 8.90,
+        "description": "Original aus dem Pizzaofen bei 500° gebacken, Tomatensauce, Büffelmozarella und Basilikum"
+    },
+    {
+        "name": "Schnitzel mit Pommes",
+        "price": 10.50,
+        "description": "Herzhaft weichgeklopftes Rindsschnitzel mit Panier, als Beilage gibt es Pommes von steirischen Bauern, alternativ auch Erdäpfel oder Salat"
+    },
+    {
+        "name": "",
+        "price": number,
+        "description": ""
+    },
+    {
+        "name": "",
+        "price": number,
+        "description": ""
+    }
+]
+
+let sideDishes = [
+    {
+        "name": "",
+        "price": number,
+        "description": ""
+    },
+    {
+        "name": "",
+        "price": number,
+        "description": ""
+    },
+]
+
+let desserts = [
+    {
+        "name": "",
+        "price": number,
+        "description": ""
+    },
+    {
+        "name": "",
+        "price": number,
+        "description": ""
+    },
+]
