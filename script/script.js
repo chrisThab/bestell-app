a639ebdcc0bd33d9df22cbf93c079932fab0e5be
chrisThab <christianthabar@gmail.com>
2025-06-21 18:11:52 +0200

    changed a lot at script.js some styles added

--- script/script.js
+++ script/script.js
@@ -1,24 +1,26 @@
-function showSidebar() {
+function showSidebar() 
+{
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'flex';
 }
 
-function hideSidebar() {
+function hideSidebar() 
+{
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'none';
 }
 
-
-function getDishes(element) {
+function getDishes(element) 
+{
     let theDish = document.getElementById("allDishes");
     theDish.innerHTML += `
     <div id="dishes">
-        <div id="oneDish">
+        <div id="oneDish" onclick="addToBasket(${element.id})">
             <p>"${element.name}"</p>
             <p>${element.description}</p>
-            <p>${element.price} €</p>
+            <p>${element.price.toFixed(2)} €</p>
         </div>
-        <div id="addbtn" onclick="addToBasket()">
+        <div id="addbtn" onclick="addToBasket(${element.id})">
         </div>
     </div>    
     `
@@ -35,15 +37,16 @@ getEveryDish();
 
 let beilagen = document.getElementById("sideDishes");
 
-function getSideDishes() {
+function getSideDishes() 
+{
     sideDishes.forEach(element => {
         beilagen.innerHTML += `<div id="dishes">
-        <div id="oneDish">
+        <div id="oneDish" onclick="addToBasket(${element.id})">
             <p>"${element.name}"</p>
             <p>${element.description}</p>
-            <p>${element.price} €</p>
+            <p>${element.price.toFixed(2)} €</p>
         </div>
-        <div id="addbtn" onclick="addToBasket()">
+        <div id="addbtn" onclick="addToBasket(${element.id})">
         </div>
     </div>`
     });
@@ -52,13 +55,41 @@ getSideDishes();
 
 //dessert
 
+function getDesserts()
+{
+    dessert = document.getElementById("desserts");
+    for (let index = 0; index < desserts.length; index++) {
+        dessert.innerHTML += `
+        <div id="dishes">
+            <div id="oneDish" onclick="addToBasket(${desserts[index].id})">
+                <p>"${desserts[index].name}"</p>
+                <p>${desserts[index].description}</p>
+                <p>${desserts[index].price.toFixed(2)} €</p>
+            </div>
+            <div id="addbtn" onclick="addToBasket(${desserts[index].id})"></div>
+        </div>`
+    }
+}
+getDesserts();
+
 //addbtn
 
-let basket = [];
-basket = document.getElementById("basket");
-
-function addToBasket() {
-    if (basket === null) {
-        return basket.map((dishes, idOfDishes))
+function addToBasket(id)
+{
+    const articles = document.getElementById("articles");
+    const dish = [...dishes, ...sideDishes, ...desserts].find(item => item.id === id);
+    const cost = document.getElementById("price");
+    const total = document.getElementById("total");
+    const delivery = document.getElementById("delivery");
+    let counter = 0;
+    const taxes = 1.2;
+    let totalPrice;
+    
+    if (dish) {
+        articles.innerHTML += ` <div> <p>"${dish.name}"</p> </div> `;
+        cost.innerHTML += `<div> <p>${dish.price.toFixed(2)} €</p> </div>`;
+        counter.innerHTML += `<p> Stk. </p>`
     }
+    delivery.innerHTML += `<p> Bei Lieferung: </p> <p> 2.50 € </p>`
+    total.innerHTML += ` <p> Summe Gesamt:</p> <p> ${totalPrice} € </p> `
 }
