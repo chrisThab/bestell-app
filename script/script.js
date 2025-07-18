30e4697e32800d01a3e8fe3c8e37618026267c15
chrisThab <christianthabar@gmail.com>
2025-07-18 20:51:35 +0200

    updated logic and design

--- script/script.js
+++ script/script.js
@@ -40,9 +40,9 @@ function updateBasket() {
     oneDish.innerHTML += `
       <div class='innerBasket'>
         <div id='article'>${item.name}</div>
-        <button id="minus" onclick='minusOne("${item.name}")'></button>
+        <button id="minus" onclick='minusOne("${item.name}")'><img src="./assets/icons/minus.png"></button>
         <div id='counter-${item.name}'>${item.counter}</div>
-        <button id="plus" onclick='plusOne("${item.name}")'></button>
+        <button id="plus" onclick='plusOne("${item.name}")'><img src="./assets/icons/plus.png"></button>
         <div id='price'>${multiplePrice.toFixed(2)} €</div>
       </div>`;
       totalPrice += multiplePrice;
