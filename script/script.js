fcedff778959ba6dcb296800a57988b3d4d57e41
chrisThab <christianthabar@gmail.com>
2025-07-28 18:31:47 +0200

    outsourced templates, scrollbar on small screens, widescreen content

--- script/script.js
+++ script/script.js
@@ -4,25 +4,30 @@ let delivery = 2.5;
 let basketArticles = {};
 let starRating = 4.2;
 
-// navbar
-function showSidebar() {
-  const sidebar = document.querySelector(".sidebar");
-  sidebar.style.display = "flex";
+// navbar toggle
+function toggleSidebar() {
+  const sidebar = document.querySelector(".sidebar")
+  if(sidebar.style.display === "none" || sidebar.style.display === ''){
+    sidebar.style.display = "block";
+  } else if (sidebar.style.display === "block"){
+    sidebar.style.display = "none";
+  };
 };
 
+// navbar display none if clicked outside
 function hideSidebar() {
-  const sidebar = document.querySelector(".sidebar");
-  sidebar.style.display = "none";
+  let sidebar = document.querySelector('.sidebar')
+  if (sidebar.style.display === "block"){
+    sidebar.style.display = "none";
+  };
 };
 
 // add to basket
 function intoBasket(dishes) {
   let maxWidth = window.innerWidth;
   if (maxWidth <= 1020) {
-    document.getElementById('two').style.display = 'none';
-  } else {
-    document.getElementById('two').style.display = 'block';
-  };
+    document.getElementById('two').style.display = 'none';} else {
+    document.getElementById('two').style.display = 'block';};
   document.getElementById('shoppingBasket').style.display ='flex';
   if (basketArticles[dishes.name]) {
     basketArticles[dishes.name].counter++;
@@ -30,8 +35,7 @@ function intoBasket(dishes) {
     name: dishes.name,
     counter: 1,
     price: dishes.price
-    };
-  };
+    };};
 updateBasket();
 };
 
@@ -44,7 +48,7 @@ function updateBasket() {
   for (let key in basketArticles) {
     let item = basketArticles[key];
     let multiplePrice = item.price * item.counter;
-    basketBuilder(oneDish, item.name, item.counter, multiplePrice);
+    builder(oneDish, item.name, item.counter, multiplePrice);
     counterSum += item.counter;
     totalPrice += multiplePrice;};
   totalPrice += delivery;
@@ -52,21 +56,6 @@ function updateBasket() {
   priceForAllDishes(totalPrice);
   checkBasketSize(counterSum);
   checkVisibility(totalPrice);
-};
-
-function basketBuilder(oneDish, itemName, itemCounter, multiplePrice){
-  oneDish.innerHTML += `<div class='innerBasket'>
-    <div class="upper">        
-        <div id='article'>${itemName}</div>
-        <div id='price'>${multiplePrice.toFixed(2)} €</div>
-    </div>
-    <div class="lower">
-        <button id="minus" onclick='minusOne("${itemName}")'><img src="./assets/icons/minus.png"></button>
-        <div id='counter-${itemName}'>${itemCounter}</div>
-        <button id="plus" onclick='plusOne("${itemName}")'><img src="./assets/icons/plus.png"></button>
-        <div id="bin" onclick='moveToBin("${itemName}")'><img src="./assets/icons/bin.png" alt="Mistkorb"></div></div>
-    </div>
-    </div>`
 };
 
 // buttons plus minus & delete
@@ -99,7 +88,7 @@ function moveToBin(name) {;
   updateBasket();
 };
 
-// bestellung abschließen
+// finish the order
 function buyArticles() {
   document.getElementById('buy').innerText = 'Wir haben ihre Bestellung entgegengenommen ! \n Sie werden zur Zahlung weitergeleitet.';
   setTimeout(()=>{
@@ -116,31 +105,14 @@ function buyArticles() {
 // basket toggle / visibility
 function toggleBasket(){
   if(document.getElementById('two').style.display =='block')
-    {document.getElementById('two').style.display ='none'}
+    {document.getElementById('two').style.display ='none';
+      document.getElementById('outerBasket').style.display ='none'
+    }
   else if(document.getElementById('two').style.display =='none')
-    {document.getElementById('two').style.display ='block'};
+    {document.getElementById('two').style.display ='block';
+      document.getElementById('outerBasket').style.display ='block'
+    };
 };
-
-function checkVisibility(totalPrice) {
-  let maxWidth = window.innerWidth;
-  if (maxWidth >= 1020 && document.getElementById('two').style.display == 'block'){
-    document.getElementById('two').style.display = 'block';
-  };
-  if (maxWidth <= 1019 && document.getElementById('two').style.display == 'none'){
-    document.getElementById('two').style.display = 'none';
-    document.getElementById('shoppingBasket').style.display = 'flex';
-  };
-  if (maxWidth <= 1019 && document.getElementById('two').style.display == 'block'){
-    document.getElementById('two').style.display = 'block';
-    document.getElementById('shoppingBasket').style.display = 'flex';
-  };
-  if(totalPrice <= 2.5){
-    document.getElementById('shoppingBasket').style.display ='none';
-    document.getElementById('two').style.display = 'none';
-  };
-};
-
-// totalPrice > 2.5 && 
 
  // get all dishes
 function displayDishes(dishArray, targetDivId) {
@@ -148,18 +120,13 @@ function displayDishes(dishArray, targetDivId) {
   targetDiv.innerHTML = '';
   for (let index = 0; index < dishArray.length; index++){
     let element = dishArray[index];
-    targetDiv.innerHTML += `
-    <div id="oneDish" onclick='intoBasket(${JSON.stringify(element)})'>
-      <div class="articleStructure"><div id="theDish">"${element.name}"</div>
-      <div id="theDescription">${element.description}</div>
-      <div id="thePrice">${element.price.toFixed(2)} €</div></div>
-      <div class="addTo"><img src="./assets/icons/plus.png" alt="plusButton"></div>`
+    targetDiv.innerHTML += display(element);
   };};
 displayDishes(dishes, "allDishes");
 displayDishes(sideDishes, "sidedishes");
 displayDishes(desserts, "dessert");
 
-// auslagerungen
+// small stuff / outsource
 function stars(){
   document.getElementById('rating').innerText =  ` ` + `${starRating} von 5 Sternen`
 };
@@ -178,5 +145,12 @@ function checkBasketSize(counterSum){
     document.getElementById('counterSize').innerHTML = counterSum;
   } else if(counterSum == 0){
     document.getElementById('counterSize').innerHTML = '';
+  };
+};
+
+function checkVisibility(totalPrice){
+  if(totalPrice <= 2.5){
+    document.getElementById('shoppingBasket').style.display ='none';
+    document.getElementById('two').style.display = 'none';
   };
 };
